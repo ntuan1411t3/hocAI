bb06019146220cd53d638bd8c7297212d091463a
Ubuntu <mrhieutrieu@gmail.com>
2017-11-01 16:57:15 +0000

    chinh file opencv

--- testopencv.py
+++ testopencv.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('watch.jpg',cv2.IMREAD_GRAYSCALE)
 
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+#plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 # plt.plot([200,300,400],[100,200,300],'c', linewidth=5)
-plt.show()
+#plt.show()
+
+print("hello world")
